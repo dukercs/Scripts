dc0c10058d385c6aff38c7305eb87ee1b93fe2d1
Rodolpho Costa Stach <dukercs@gmail.com>
2021-12-23 19:24:44 -0300

    alterado curl

--- Shell/healthcheck/saudevalida.sh
+++ Shell/healthcheck/saudevalida.sh
@@ -24,7 +24,7 @@ f_curl(){
         tsistema=$(expr $(date +%s) - $(stat -c %Y /proc/uptime))
     done
 
-    $cURL -o /dev/null -L -f --retry 6 --max-time 5 --retry-delay 10 --retry-max-time 60 $site
+    $cURL curl -m 15 -s -o /dev/null -L -w "%{http_code}" $site > /tmp/saudevalida.txt 2>&1
     if test $? -ne 0 
     then
         kill -9 -1
